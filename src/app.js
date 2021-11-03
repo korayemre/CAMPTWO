b0d93d389883b9f867a6bb837bcec7a039d5750d
koray emre <koraykazan@hotmail.com>
2021-11-04 01:36:37 +0300

    2. ders ödev

---
+++ src/app.js
@@ -0,0 +1,20 @@
+console.log("merhaba kodlama dendfdf")
+var dolarDun = 9.20
+dolarDun = 'ankara'
+var dolarbugun = 9.30
+console.log(dolarDun)
+const eurodun = 11
+let DENEME = 15
+{
+//DENEME = 'vxvcv'
+
+
+}
+console.log(DENEME)
+let konutKredileri = ['konut Kredisi', 'Emlak Konut Kredisi' , 'Kamu Konut Kredisi']
+//console.log(konutKredileri[2])
+for(let  i = 0;i<konutKredileri.length;i++)
+{
+    console.log("<li>" + konutKredileri[i] + "</li>")
+
+}
